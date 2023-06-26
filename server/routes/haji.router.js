70a49595e018bb1f89a628b3ba30ae3f936e006c
Majid Igale <majidigale18@gmail.com>
2023-06-26 12:28:45 -0500

    created server side GET request

--- server/routes/haji.router.js
+++ server/routes/haji.router.js
@@ -6,7 +6,17 @@ const router = express.Router();
  * GET route template
  */
 router.get('/', (req, res) => {
-  // GET route code here
+  // GET route code for to pull data from haji_database
+  pool.query(`SELECT * FROM "hajj"`)
+
+  .then(result => {
+    console.log('Whats coming from database?', result.rows)
+    res.send(result.rows)
+  })
+  .catch(err => {
+    console.log('Error with Server GET', err)
+    res.sendStatus(500)
+  })
 });
 
 /**

bc2f4d6fba75ab9c3e19984331729880f2ad4b00
Majid Igale <majidigale18@gmail.com>
2023-06-26 16:07:54 -0500

    created watcher function for fetchHji saga and prepped it for export

--- src/redux/sagas/hajjTable.saga.js
+++ src/redux/sagas/hajjTable.saga.js
@@ -15,3 +15,9 @@ function* fetchHajj(){
         console.log('Error with GET request ClientSide', err)
     }
 }
+
+function* hajiFetch() {
+    yield takeLatest('FETCH_HAJI', fetchHajj) 
+}
+
+export default hajiFetch

724182d44b8e6994f6e2565073946b4d1ff5e78f
Majid Igale <majidigale18@gmail.com>
2023-06-28 15:06:00 -0500

    added link to budget page on Nav Component

---
+++ src/components/HajiTable/HajiTable.jsx
@@ -0,0 +1,10 @@
+
+
+function HajiTable() {
+
+    return (
+        <p>Under Construction Progress Page</p>
+    )
+}
+
+export default HajiTable
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -31,6 +31,10 @@ function Nav() {
             <Link className="navLink" to="/info">
               Info Page
             </Link>
+            <Link className="navLink" to="/budget">
+              Budget
+              
+            </Link>
 
             <LogOutButton className="navLink" />
           </>

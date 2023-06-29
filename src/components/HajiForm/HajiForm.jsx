fc5f12ff6d55cf68f4f19f9d241c0ca62056cd71
Majid Igale <majidigale18@gmail.com>
2023-06-29 18:46:28 -0500

    created form logic

--- src/components/HajiForm/HajiForm.jsx
+++ src/components/HajiForm/HajiForm.jsx
@@ -8,10 +8,32 @@ function HajiForm(){
     let [saved, setSaved] = useState(0)
 
     return (
+        <>
         <form>
-
+            <label>Budget Name</label>
+            <input 
+            type="text"
+            placeholder='Budget-Name'
+            value={name}
+            onChange={(e) => setName(e.target.value)} />
+            <label>Budget</label>
+            <input 
+            type="number"
+            placeholder='Goal'
+            value={budgetAmount}
+            onChange={(e) => setBudget(e.target.value)} />
         </form>
-    
+
+        <form>
+        <label>Amount Saved</label>
+        <input 
+        type='number'
+        placeholder='Amount'
+        value={saved}
+        onChange={(e) => setSaved(e.target.value)} />
+        <button>Add</button>
+        </form>
+        </>
     )
 }
 

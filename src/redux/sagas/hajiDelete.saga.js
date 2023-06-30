8e47f337644d9cd894ad7118a9059b7c2be0e1f3
Majid Igale <majidigale18@gmail.com>
2023-06-30 10:49:12 -0500

    created generator function for DELETE SAGA

--- src/redux/sagas/hajiDelete.saga.js
+++ src/redux/sagas/hajiDelete.saga.js
@@ -1,2 +1,14 @@
 import axios from 'axios';
 import {put, takeLatest} from 'redux-saga/effects';
+
+function* deleteTableItem(action){
+    try{
+
+        yield axios.delete(`/api/haji/${action.payload.id}`)
+        yield put({
+            type: 'FETCH_HAJI'
+        });
+    } catch(err) {
+        console.log('DELETE ERROR ===>', err)
+    }
+}
